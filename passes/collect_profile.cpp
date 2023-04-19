5ea80439a4f7c7a24fbb9f04d6e072d18715c7b6
Neel Shah <neelsh@eecs583a.eecs.umich.edu>
2023-04-19 15:14:05 -0400

    Create LLVM pass to insert printf before and after relevant function calls

--- passes/collect_profile.cpp
+++ passes/collect_profile.cpp
@@ -1,6 +1,9 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IRBuilder.h"
 
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
@@ -13,9 +16,44 @@ struct Hello : public FunctionPass {
   Hello() : FunctionPass(ID) {}
 
   bool runOnFunction(Function &F) override {
-    errs() << "Hello: ";
-    errs().write_escaped(F.getName()) << '\n';
-    return false;
+    Module *M = F.getParent();
+    // TODO: figure out how to not need to hardcode the function.
+    Function *func = M->getFunction("exp");
+    LLVMContext & context = M->getContext();
+    Type *intType = Type::getInt32Ty(context);
+    // Declare C standard library printf 
+    std::vector<Type *> printfArgsTypes({Type::getInt8PtrTy(context)});
+    FunctionType *printfType = FunctionType::get(intType, printfArgsTypes, true);
+    auto print = M->getOrInsertFunction("printf", printfType);
+    if (!func) {
+      errs() << "Error\n";
+    }
+
+    for (BasicBlock &BB : F) {
+      for (Instruction &I : BB) {
+        if (CallInst *call = dyn_cast<CallInst>(&I)) {
+          if (call->getCalledFunction() == func) {
+            Value *x = call->getArgOperand(0);
+            Value *y = dyn_cast<Value>(call);
+            IRBuilder<> builder(call);
+            builder.SetInsertPoint(call);
+            builder.CreateCall(print, {builder.CreateGlobalStringPtr("%f "), x});
+            builder.SetInsertPoint(call->getNextNonDebugInstruction());
+            builder.CreateCall(print, {builder.CreateGlobalStringPtr("%f "), y});
+
+          }
+        }
+      }
+    }
+    // For debug purposes only.
+    for (BasicBlock &BB : F) {
+      errs() << "===============================\n";
+      for (Instruction &I : BB) {
+        errs() << I << "\n";
+      }
+      errs() << "===============================\n";
+    }
+    return true;
   }
 }; // end of struct Hello
 }  // end of anonymous namespace
