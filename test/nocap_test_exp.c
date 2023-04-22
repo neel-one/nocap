a49c4035a3176fae4f4c5ca3488cf86154430ed1
neel-one <thensred00@gmail.com>
2023-04-22 15:35:44 -0400

    Added a favorable benchmark

--- test/nocap_test_exp.c
+++ test/nocap_test_exp.c
@@ -1,11 +1,11 @@
-#include <math.h>
 #include <stdio.h>
+#include "../build/src/nocap_exp.h"
 
 int main() {
 for (int j = 0; j < 1e9; j++) {
     for (int i = 0; i <= 20; i++) {
         double x = i;
-        double y = exp(-x);
+        double y = nocap_exp(-x);
     }
 }
     return 0;
