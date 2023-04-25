2a4885c6436c7eae195f89b0e61ccbd4ffb53b4d
Peter Maxwell Ly <pmly@eecs583a.eecs.umich.edu>
2023-04-25 10:59:17 -0400

    Add smaller tests for basic functions that run faster

---
+++ test/small_test_sqrt/small_test_sqrt.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+for (int j = 0; j < 1e4; j++) {
+    for (int i = 0; i <= 1000; i++) {
+        double x = i;
+        double y = sqrt(x);
+    }
+}
+    return 0;
+}
